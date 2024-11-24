dd7acc7d88278f560595ca918e048f62624a9574
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-24 18:51:34 +0300

    feat: Add About component with introductory content and styling

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div id="about" className="min-h-screen w-screen ">
+        <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+          <h2 className="font-general text-sm uppercase md:text-[10px]">
+            Welcome to Zentry
+          </h2>
+
+          <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver the world&apos;s l<b>a</b>rgest shared adventure
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
