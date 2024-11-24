8bc29ce73bf1dbca77164717dd73fe8843be485b
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-25 00:03:13 +0300

    feat: Add navItems, buttons to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
 import { useRef } from "react"
+import Button from "./Button"
+import { TiLocationArrow } from "react-icons/ti"
+
+const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
 const Navbar = () => {
     const navContainerRef = useRef(null)
@@ -13,6 +17,18 @@ const Navbar = () => {
                   alt="logo"
                   className="w-10"
                 />
+                <Button
+                  id="product-button"
+                  title="Products"
+                  rightIcon={<TiLocationArrow />}
+                  containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+                />
+             </div>
+
+             <div className="flex h-full items-center">
+                <div className="hidden md:block">
+
+                </div>
              </div>
            </nav>
         </header>
