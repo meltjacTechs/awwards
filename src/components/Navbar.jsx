48f9389f2edaa9d322ee3d0807336e87a2023c93
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-25 10:43:56 +0300

    feat: Add audio toggle functionality and state management to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
 
-const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
+    const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+
+    const [isIndicatorActive, setIsIndicatorActive] = useState(false)
+
     const navContainerRef = useRef(null)
+
+    const audioElementRef = useRef(null)
+  
+    const toggleAudioIndicator = () => {
+
+    }
 
   return (
     <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
@@ -27,8 +37,22 @@ const Navbar = () => {
 
              <div className="flex h-full items-center">
                 <div className="hidden md:block">
-
+                    {navItems.map((item) => (
+                        <a key={item} href={`#${item.toLowerCase()}`} className="nav-hover-btn">
+                            {item}
+                        </a>
+                    ))}
                 </div>
+
+                <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}>
+                  <audio ref={audioElementRef} className="hidden" src="/audio/loop.mp3" loop>
+                     {[1, 2, 3, 4].map((bar) => (
+                      <div key={bar} className={`indicator-line`}>
+
+                      </div>
+                     ))}
+                  </audio>
+                </button>
              </div>
            </nav>
         </header>
