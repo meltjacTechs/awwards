3e7ee8d8ce907af1f7120427459d151e5837c434
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-26 22:30:41 +0300

    feat: Implement scroll-based visibility toggle for Navbar and enhance audio control functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,64 @@
-import { useRef, useState } from "react"
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
+import { useWindowScroll } from "react-use"
 
 const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const [isNavVisible, setIsNavVisible] = useState(true);
+  
     const [isAudioPlaying, setIsAudioPlaying] = useState(false)
 
     const [isIndicatorActive, setIsIndicatorActive] = useState(false)
 
-    const navContainerRef = useRef(null)
+    const navContainerRef = useRef(null);
 
     const audioElementRef = useRef(null)
-  
-    const toggleAudioIndicator = () => {
 
+    const { y: currentScrollY } = useWindowScroll();
+
+    useEffect(() => {
+      if(currentScrollY === 0) {
+        setIsNavVisible(true);
+        navContainerRef.current.classList.remove('floating-nav');
+      }else if (currentScrollY > lastScrollY) {
+        setIsNavVisible(false);
+        navContainerRef.current.classList.add('floating-nav')
+      }
+      else if (currentScrollY < lastScrollY) {
+        setIsNavVisible(true);
+        navContainerRef.current.classList.add('floating-nav')
+      }
+
+      setLastScrollY(currentScrollY)
+    }, [currentScrollY, lastScrollY])
+
+    useEffect(() => {
+      gsap.to(navContainerRef, {
+        y: isNavVisible ? 0 : -100,
+        opacity: isNavVisible ? 1 : 0,
+        duration: 0.2,
+      })
+    }, [isNavVisible])
+
+    const toggleAudioIndicator = () => {
+      setIsAudioPlaying((prev) => !prev);
+
+      setIsIndicatorActive((prev) => !prev);
     }
+
+    useEffect(() =>{
+      if(isAudioPlaying) {
+        audioElementRef.current.play();
+      }else {
+        audioElementRef.current.pause();
+      }
+
+    }, [isAudioPlaying])
 
   return (
     <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
@@ -45,13 +88,12 @@ const Navbar = () => {
                 </div>
 
                 <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}>
-                  <audio ref={audioElementRef} className="hidden" src="/audio/loop.mp3" loop>
+                  <audio ref={audioElementRef} className="hidden" src="/audio/loop.mp3" loop />
+                     
                      {[1, 2, 3, 4].map((bar) => (
-                      <div key={bar} className={`indicator-line`}>
-
-                      </div>
+                      <div key={bar} className={`indicator-line ${isIndicatorActive ? 'active' : ''}`} style={{animationDelay: `${bar * 0.1}s`}} />
                      ))}
-                  </audio>
+
                 </button>
              </div>
            </nav>
