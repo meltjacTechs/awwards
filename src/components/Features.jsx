fa9c4c716a8aed2a76468c488344580793f06bd4
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-27 11:50:18 +0300

    feat: Refactor Features component layout for improved structure and readability

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,7 +12,9 @@ const BentoCard = ({ src, title, description, isComingSoon }) => {
         <div>
           <h1 className="bento-title special-font">{title}</h1>
           {description && (
-            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+            <p className="mt-3 max-w-64 text-xs md:text-base">
+              {description}
+            </p>
           )}
         </div>
       </div>
@@ -26,13 +28,12 @@ const Features = () => {
       <div className="container mx-auto px-3 md:px-10">
         <div className="px-5 py-32 ">
           <p className="font-circular-web text-lg text-blue-50">Into the Metagame</p>
-        </div>
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
           Immerse yourself in a rich and ever-expanding universe where a
           vibrant array of products converge into an interconnected overlay
           experience on your world.
         </p>
-      </div>
+        </div>
       <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
         <BentoCard
          src="videos/feature-1.mp4"
@@ -43,6 +44,7 @@ const Features = () => {
          across web2 and web3 games into a rewarding adventure"
          isComingSoon={true}
         />
+      </div>
       </div>
     </section>
   )
