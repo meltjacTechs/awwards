4aba5eb89404362a2eb69835a812f55a40349bbc
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-27 14:20:36 +0300

    feat: Enhance Story component with AnimatedTitle and improved layout for better presentation

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,19 @@
+import AnimatedTitle from './AnimatedTitle';
+
 const Story = () => {
   return (
-    <div>Story</div>
-  )
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">the multiversal ip world</p>
+
+        <div className="relative size-full ">
+            <AnimatedTitle
+             title="The story of a hidden realm" 
+            />
+        </div>
+      </div>
+    </section>
+ )
 }
 
 export default Story
