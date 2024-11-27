7cb56d6b381d3de85dbe792a5bf9cfa93a2b3ad4
Emmanuel Juma Bwire <ebwire02@gmail.com>
2024-11-27 12:16:16 +0300

    feat: Add BentoCard component 2 to Features section for enhanced content display

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -26,7 +26,7 @@ const Features = () => {
   return (
     <section className="bg-black pb-52 ">
       <div className="container mx-auto px-3 md:px-10">
-        <div className="px-5 py-32 ">
+        <div className="px-5 py-32">
           <p className="font-circular-web text-lg text-blue-50">Into the Metagame</p>
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
           Immerse yourself in a rich and ever-expanding universe where a
@@ -44,6 +44,19 @@ const Features = () => {
          across web2 and web3 games into a rewarding adventure"
          isComingSoon={true}
         />
+      </div>
+      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+        <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <BentoCard 
+            src="videos/feature-2.mp4"
+            title={
+            <>
+             zig<b>m</b>a
+            </>
+            }
+            description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+          />
+        </div>
       </div>
       </div>
     </section>
